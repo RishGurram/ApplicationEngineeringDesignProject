218ee9736229ce10dee8314690048046cfef0785
Rishika Gurram <gurram.ri@northeastern.edu>
2023-05-08 02:01:32 -0400

    Aed project

---
+++ FinalProject-main/src/MODEL/Doctor.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODEL;
+
+/**
+ *
+ * @author Aish
+ */
+public class Doctor {
+    
+    public static int doc_id; 
+
+    public static int getDoc_id() {
+        return doc_id;
+    }
+
+    public static void setDoc_id(int doc_id) {
+        Doctor.doc_id = doc_id;
+    }
+    
+    
+}
---
+++ FinalProject-main/src/MODEL/HOSPITAL.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODEL;
+
+/**
+ *
+ * @author Aish
+ */
+public class HOSPITAL {
+    public static int hos_id ; 
+
+    public static int getHos_id() {
+        return hos_id;
+    }
+
+    public static void setHos_id(int hos_id) {
+        HOSPITAL.hos_id = hos_id;
+    }
+    
+}
